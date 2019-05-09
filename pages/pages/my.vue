6fb113e44be089ac483270c2c9048f753c9c5fb1
365556520 <365556520@qq.com>
2019-05-09 11:27:23 +0800

    底部导航

---
+++ pages/pages/my.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title"  @click="goto()">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '我是我的'
+			}
+		},
+
+		onLoad: function (option) { //option为object类型，会序列化上个页面传递的参数
+			console.log(option.id); //打印出上个页面传递的参数。
+			console.log(option.name); //打印出上个页面传递的参数。
+		},
+		methods: {
+			goto(){
+				uni.navigateTo({
+					url: '/pages/index/index'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
