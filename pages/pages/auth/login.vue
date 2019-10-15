b7db7f8914a864d364598da2631932d1e6dc49df
365556520 <365556520@qq.com>
2019-10-15 17:36:55 +0800

    实现第三方登录

--- pages/pages/auth/login.vue
+++ pages/pages/auth/login.vue
@@ -11,15 +11,16 @@
 			</view>
 			
 			<view class="padding flex flex-direction">
-				<button class="cu-btn bg-red margin-tb-sm lg" @click="login()">登 录</button>
+				<button v-show="!correlationShow" class="cu-btn bg-red margin-tb-sm lg" @click="login()">登 录</button>
+				<button v-show="correlationShow" class="cu-btn bg-red margin-tb-sm lg" @click="correlation()">绑定此账号</button>
 			</view>
 			<view class="action-row">
 			    <navigator url="/pages/common/commonurl?url=http://www.diziw.cn/register&title=账号注册">注册账号</navigator>
 			    <text>|</text>
 			    <navigator url="/pages/pages/auth/retrieve">忘记密码</navigator>
 			</view>
-			<view class="padding ">
-				<view class="text-center padding">————第三方登录————</view>
+			<view class="padding " v-show="correlationShow">
+				<view class="text-center padding">其他登录方式</view>
 				<view class="oauth-row">
 					<view class="oauth-image" @click="providerlogin('qq')">
 						<image src="../../../static/qq.png" ></image>
@@ -30,7 +31,6 @@
 				</view>
 			</view>
 		</form>
-		
 	</view>
 </template>
 
@@ -45,8 +45,8 @@
 				logindata:{
 					'username':'',
 					'password':''
-				}
-			
+				},
+				correlationShow:false,//第三方登录关联显示
 			}
 		},
 		onLoad: function (option) { //option为object类型，会序列化上个页面传递的参数
@@ -62,10 +62,10 @@
 			    'setName'
 				
 			]),
-			login(){
-				/*  if (/^[a-zA-Z0-9]{2,15}$/.test(value))
-				*  if (/^[a-zA-Z0-9]{6,15}$/.test(value))
-				*  */
+			/*  if (/^[a-zA-Z0-9]{2,15}$/.test(value))
+			*  if (/^[a-zA-Z0-9]{6,15}$/.test(value))
+			*  */
+			login(){ //输入账号密码登录
 				let data = {
 					username:this.logindata.username,
 					password:this.logindata.password,
@@ -74,19 +74,7 @@
 				this.$api.post('/api/login',data).then((res)=>{
 					let userData=JSON.parse(res.data);//把json转换数组
 					if(res.statusCode=='200'){
-						 this.setToken(userData.token);//把token保存到vuex里面
-						 //获取用户信息
-						 this.getuserdata(this.userToken);
-						 uni.showToast({
-							 title: userData.message,
-							 icon: 'success',
-							 mask: true
-						 });
-						 uni.navigateBack({
-							delta: 1, //返回上一页
-							animationType: 'pop-out', //动画
-							animationDuration: 300 //动画时间
-						});
+						this.successLogin(userData);
 						// console.log('打印token', uni.getStorageSync('userToken'));
 					}
 				  //  this.res = '请求结果 : ' + JSON.stringify(res);
@@ -115,6 +103,7 @@
 				}
 			},
 			providerlogin(provider){
+				let this_ =this;
 				uni.login({
 					provider: provider,
 					success: (resp) => {
@@ -122,26 +111,61 @@
 						uni.getUserInfo({
 						    provider: provider,
 						    success: function (infoRes) {
-								console.log('测试微博数据昵称'+infoRes.userInfo.nickname)
-								console.log('测试微博数据性别'+infoRes.userInfo.gender)
-								console.log('测试微博数据id'+infoRes.userInfo.openid)
-								console.log('测试微博数据图标地址'+infoRes.userInfo.avatarUrl)
-								//console.log('测试微博数据'+access_token)
-								var formdata={
+								/* console.log('测试数据昵称---'+infoRes.userInfo.nickname)
+								console.log('测试数据性别---'+infoRes.userInfo.gender)
+								console.log('测试数据id---'+infoRes.userInfo.openId)
+								console.log('测试数据图标地址---'+infoRes.userInfo.avatarUrl) */
+								let gender = infoRes.userInfo.gender; //因为微博数据不一样
+								if(provider=='sinaweibo'){
+									gender = infoRes.userInfo.gender=='m'?'男':'女';
+								}
+								var data={
 									nickName:infoRes.userInfo.nickname,
-									gender:infoRes.userInfo.gender=='男'?0:1,
-									openId:infoRes.userInfo.openid,
-									provider:provider,
+									gender:gender,	
+									provider_id:infoRes.userInfo.openId,
+									provider:provider=='sinaweibo'?'weibo':provider,
+									avatarUrl:infoRes.userInfo.avatarUrl
 									//access_token:access_token,
 								};
-								//self.$go.post("/qqlogin",formdata).then(res=>{});
+								//第三方后台登录
+								this_.$api.post('/api/social/login',data).then((res)=>{
+									let userData=JSON.parse(res.data);//把json转换数组
+									if(res.statusCode=='200'){
+										 this_.successLogin(userData);
+										// console.log('打印token', uni.getStorageSync('userToken'));
+									}
+								}).catch((err)=>{
+									let data = JSON.parse(err.data);
+									//显示绑定此账号
+									this_.correlationShow = true;
+								    //改变登录框内容
+									console.log('绑定已有账号', data);
+								})
 							}
 						})
 					},
 					fail: (err) => {}
 				});
+			},
+			//登录成功后方法
+			successLogin(userData){
+				this.setToken(userData.token);//把token保存到vuex里面
+				 //获取用户信息
+				 this.getuserdata(this.userToken);
+				 uni.showToast({
+					 title: userData.message,
+					 icon: 'success',
+					 mask: true
+				 });
+				 uni.navigateBack({
+					delta: 1, //返回上一页
+					animationType: 'pop-out', //动画
+					animationDuration: 300 //动画时间
+				});
+			},
+			correlation(){
+				//绑定已有账号逻辑
 			}
-
 		}
 	}
 </script>
